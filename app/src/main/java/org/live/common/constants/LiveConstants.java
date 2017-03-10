e22c294f39d69c7dfbe4c7a84818a1a0973871a6
wang007 <704450642@qq.com>
2017-03-10 19:52:19 +0800

    全局常量

---
+++ app/src/main/java/org/live/common/constants/LiveConstants.java
@@ -0,0 +1,37 @@
+package org.live.common.constants;
+
+/**
+ *  全局的常量定义
+ *
+ * Created by Mr.wang on 2017/3/9.
+ */
+public class LiveConstants {
+
+    /**
+     * 远程服务器地址
+     */
+    public static final String REMOTE_SERVER_IP = "127.0.0.1" ;
+
+    /**
+     *  http协议开头
+     */
+    public static final String HTTP_PREFIX = "http://" ;
+
+    /**
+     *  rtmp协议开头
+     */
+    public static final String REMP_PREFIX = "rtmp://" ;
+
+    /**
+     *  hls直播协议地址的后缀
+     */
+    public static final String HLS_TYPE_SUFFIX = ".m3u8" ;
+
+    /**
+     * flv直播协议地址的后缀
+     */
+    public static final String FLV_TYPE_SUFFIX = ".flv" ;
+
+
+
+}
---
+++ app/src/main/java/org/live/common/constants/LiveKeyConstants.java
@@ -0,0 +1,21 @@
+package org.live.common.constants;
+
+/**
+ *  全局的key
+ *
+ * Created by Mr.wang on 2017/3/9.
+ */
+
+public class LiveKeyConstants {
+
+    /**
+     * 播放地址的key
+     */
+    public static final String PLAY_IP_KEY = "playIpKey" ;
+
+    /**
+     *  播放格式，rtmp，flv，hls。
+     */
+    public static final String PLAY_TYPE_KEY = "playTypeKey" ;
+
+}
