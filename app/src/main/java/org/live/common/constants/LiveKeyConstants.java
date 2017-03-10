7a77d3b902b1ed0208b45ffec5521cb7ac5a111f
347255699 <13710414456@163.com>
2017-03-10 20:14:33 +0800

    添加global_url_key

--- app/src/main/java/org/live/common/constants/LiveKeyConstants.java
+++ app/src/main/java/org/live/common/constants/LiveKeyConstants.java
@@ -1,8 +1,8 @@
 package org.live.common.constants;
 
 /**
- *  全局的key
- *
+ * 全局的key
+ * <p>
  * Created by Mr.wang on 2017/3/9.
  */
 
@@ -11,11 +11,15 @@ public class LiveKeyConstants {
     /**
      * 播放地址的key
      */
-    public static final String PLAY_IP_KEY = "playIpKey" ;
+    public static final String PLAY_IP_KEY = "playIpKey";
 
     /**
-     *  播放格式，rtmp，flv，hls。
+     * 播放格式，rtmp，flv，hls。
      */
-    public static final String PLAY_TYPE_KEY = "playTypeKey" ;
+    public static final String PLAY_TYPE_KEY = "playTypeKey";
 
+    /**
+     * 全局 url key, 仅用于demo模块
+     */
+    public static final String Global_URL_KEY = "globalUrlKey";
 }
