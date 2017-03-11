99c26adbb5f37ab139473e5eefcaf00c6b96b422
wang007 <704450642@qq.com>
2017-03-11 15:04:35 +0800

    mainActivity代码

--- app/src/main/java/org/live/module/demo/MainActivity.java
+++ app/src/main/java/org/live/module/demo/MainActivity.java
@@ -6,22 +6,35 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
 import org.live.R;
 import org.live.common.constants.LiveKeyConstants;
+import org.live.module.play.view.impl.PlayActivity;
+import org.live.module.publish.view.impl.PublishActivity;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
     private static final String TAG = "MainActivity";
     private ListView mDemoListView = null;
+
+    private EditText inputPushIp = null ;   //推流输入框
+
+    private EditText inputPlayIp = null ;   //拉流输入框
+
+    private Button liveBtn = null ; //直播按钮
+
+    private Button recordBtn = null ;   //录播按钮
+
+    private Button playBtn = null  ;    //播放按钮
 
     private static final String[] M_ITEM_TITLES = {
             "推流界面",
@@ -65,6 +78,13 @@ public class MainActivity extends Activity {
         //添加并且显示
         mDemoListView.setAdapter(adapter);
         mDemoListView.setOnItemClickListener(listener);
+
+        inputPushIp = (EditText) this.findViewById(R.id.et_main_inputPushIp) ;
+        inputPlayIp = (EditText) this.findViewById(R.id.et_mian_inputPlayIp) ;
+        this.findViewById(R.id.btn_main_play).setOnClickListener(this) ;
+        this.findViewById(R.id.btn_main_live).setOnClickListener(this) ;
+        this.findViewById(R.id.btn_main_record).setOnClickListener(this) ;
+
     }
 
     /**
@@ -81,6 +101,29 @@ public class MainActivity extends Activity {
             list.add(map);
         }
         return list;
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent() ;
+        int btnFlag = v.getId() ;
+        switch (btnFlag) {
+            case R.id.btn_main_play: {
+                intent.putExtra(LiveKeyConstants.Global_URL_KEY, inputPlayIp.getText().toString()) ;
+                intent.setClass(this, PlayActivity.class) ;
+                break ;
+            }
+            case R.id.btn_main_live: {
+                intent.putExtra(LiveKeyConstants.Global_URL_KEY, inputPushIp.getText().toString()) ;
+                intent.setClass(this, PublishActivity.class) ;
+                break ;
+            }
+            case R.id.btn_main_record: {
+                intent.putExtra(LiveKeyConstants.Global_URL_KEY, inputPushIp.getText().toString()) ;
+                intent.setClass(this,PublishActivity.class) ;
+            }
+        }
+
     }
 
     /**
